b380bbc94c4c98bb195a52fe62d2a403507cdc77
rio.ferbiansyah@idntimes.com <rio.ferbiansyah@idntimes.com>
2019-06-21 09:02:38 +0000

    MKFILE Command Bug Fix

--- app/DOSBox/Command/Library/CmdMkFile.php
+++ app/DOSBox/Command/Library/CmdMkFile.php
@@ -2,27 +2,40 @@
 
 namespace DOSBox\Command\Library;
 
+use DOSBox\Command\BaseCommand as Command;
+use DOSBox\Filesystem\File;
 use DOSBox\Interfaces\IDrive;
 use DOSBox\Interfaces\IOutputter;
-use DOSBox\Filesystem\File;
-use DOSBox\Command\BaseCommand as Command;
 
-class CmdMkFile extends Command {
-    public function __construct($commandName, IDrive $drive){
+class CmdMkFile extends Command
+{
+    public function __construct($commandName, IDrive $drive)
+    {
         parent::__construct($commandName, $drive);
     }
 
-    public function checkNumberOfParameters($numberOfParametersEntered) {
+    public function checkNumberOfParameters($numberOfParametersEntered)
+    {
         return true;
     }
 
-    public function checkParameterValues(IOutputter $outputter) {
+    public function checkParameterValues(IOutputter $outputter)
+    {
+
+        if (!isset($this->params[0]) && empty($this->params[0])) {
+            $outputter->printLine('File name not defined');
+            return false;
+        }
         return true;
     }
 
-    public function execute(IOutputter $outputter){
+    public function execute(IOutputter $outputter)
+    {
         $fileName = $this->params[0];
-        $fileContent = $this->params[1];
+        $fileContent = null;
+        if (isset($this->params[1]) && !empty($this->params[1])) {
+            $fileContent = $this->params[1];
+        }
         $newFile = new File($fileName, $fileContent);
         $this->getDrive()->getCurrentDirectory()->add($newFile);
     }
